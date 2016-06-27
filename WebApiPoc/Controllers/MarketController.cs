f23f7360711b78dd70fc2cb7cca7c24176729967
Simone Finotti <tech178@yoox.com>
2016-06-27 12:28:54 +0200

    PUT handling slightly improved.

--- WebApiPoc/Controllers/MarketController.cs
+++ WebApiPoc/Controllers/MarketController.cs
@@ -6,7 +6,7 @@ using System.Web.Http;
 namespace WebApiPoc.Controllers
 {
     [ETagActionFilter]
-    [VersionedRoute("api/markets", 1)]
+    [RoutePrefix("api/markets")]
     public class MarketController : ApiController
     {
         private readonly IMarketRepository _marketRepository;
@@ -28,12 +28,14 @@ namespace WebApiPoc.Controllers
             _etagGenerator = etagGenerator;
         }
 
+        [Route("")]
         public HttpResponseMessage Get(HttpRequestMessage request)
         {
             var _ = request.CreateResponse(HttpStatusCode.OK, _marketRepository.GetAll());
             return _;
         }
 
+        [Route("{id:int}")]
         public HttpResponseMessage Get(HttpRequestMessage request, int id)
         {
             var market = _marketRepository.Get(id);
@@ -42,6 +44,7 @@ namespace WebApiPoc.Controllers
                 : request.CreateResponse(HttpStatusCode.OK, market);
         }
 
+        [Route("")]
         public HttpResponseMessage Post([FromBody]Market value)
         {
             var saved = _marketRepository.Store(value);
@@ -50,16 +53,20 @@ namespace WebApiPoc.Controllers
                 : new HttpResponseMessage(HttpStatusCode.Created);
         }
 
+        [Route("{id:int}")]
         public HttpResponseMessage Put(int id, [FromBody]Market value)
         {
-            var market = _marketRepository.Get(id);
-            if (market == null)
+            var r = _marketRepository.Delete(id);
+            if (!r)
                 return new HttpResponseMessage(HttpStatusCode.NotFound);
 
-            market.Name = value.Name;
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            r = _marketRepository.Store(value);
+            return r 
+                ? new HttpResponseMessage(HttpStatusCode.OK) 
+                : new HttpResponseMessage(HttpStatusCode.PaymentRequired);
         }
 
+        [Route("{id:int}")]
         public HttpResponseMessage Delete(int id)
         {
             var result = _marketRepository.Delete(id);
